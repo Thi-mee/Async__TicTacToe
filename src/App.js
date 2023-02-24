9708da6d687dd7c12ca4f6438d188a71856e5e29
Timi Adenuga <timswaggs1@outlook.com>
2023-02-24 02:15:09 +0100

    feat: implemented protectedRoutes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
-import { useState, createContext } from "react";
-import { Routes, Route } from "react-router-dom";
+import { useState, createContext, useContext } from "react";
+import { Routes, Route, Navigate, Outlet } from "react-router-dom";
 import GameRoom from "./pages/GameRoom";
 import Home from "./pages/Home";
 import Room from "./pages/Room";
+import Login from "./pages/Login";
 
 export const UserContext = createContext()
 
+export const ProtectedRoute = () => {
+  const { loggedIn } = useContext(UserContext)
+  return (
+    loggedIn ? <Outlet /> : <Navigate to="/login" />
+  )
+}
+
+
 function App() {
   const [user, setUser] = useState(null);
-  
+  const [loggedIn, setLoggedIn] = useState(false);
+
   return (
-    <UserContext.Provider value={{user, setUser}} >
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/home" element={<Room />} />
-      <Route path="/gameroom/:id" element={<GameRoom />} />
+    <UserContext.Provider value={{ user, setUser, loggedIn }} >
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login setUser={setUser} setLoggedIn={setLoggedIn} />} />
 
 
-      TODO: Redirect known routes to login page if user is not logged in
+        TODO: Redirect known routes to login page if user is not logged in
+        <Route path="/home" element={<ProtectedRoute />} >
+          <Route index element={<Room />} />
+        </Route>
+        <Route path="/gameroom/:id" element={<ProtectedRoute />} >
+          <Route index element={<GameRoom />} />
+        </Route>
 
-
-      <Route path='*' element={<h1>404 - Not Found</h1>} />
-    </Routes>
+        <Route path='*' element={<h1>404 - Not Found</h1>} />
+      </Routes>
     </UserContext.Provider>
   );
 }
